a799f1db73ca6fda499b5de4a23235f2accc1684
MANISHA BOSE <manishabose@MANISHAs-Laptop.local>
2024-07-07 19:51:47 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,52 @@
+# Book Showcase Website
+
+## Overview
+This website is a personal book showcase that displays my book collection, reviews, and ratings. It allows visitors to browse through my reading list, view book details, and read my personal takes on various books.
+
+## Technologies Used
+
+### Frontend
+- **Embedded Javascript (EJS)**: For structuring the web content, client-side interactivity and generating HTML markup with plain JavaScript.
+- **CSS**: For styling the website, including responsive design.
+- **Bootstrap 5**: Used for its grid system and pre-built components to ensure responsiveness and consistent styling across devices.
+
+### Backend
+- **Node.js**: The runtime environment for the server.
+- **Express.js**: Web application framework for Node.js, used to build the server-side of the application.
+
+### Database
+- **PostgreSQL**: An open-source object-relational database system used to store book information, reviews, and user data.
+- **node-postgres (pg)**: Non-blocking PostgreSQL client for Node.js, used to interact with the PostgreSQL database from the Node.js application.
+
+### APIs
+- **Open Library API**: Used to fetch book cover images and additional book metadata.
+
+### Deployment
+- **Render**: Cloud application hosting service used for deploying and hosting the application. Render provides easy deployment from Git repositories and supports PostgreSQL databases.
+
+## Features
+- Display of book collection with cover images, titles, and authors
+- Detailed view for each book including my personal review, rating and notes
+- Responsive design that works on desktop and mobile devices
+- Integration with Open Library API for book cover images
+
+## Setup and Installation
+1. Clone the repository:
+````bash
+git clone https://github.com/ManishaBose/Books_Read.git
+````
+2. Install the dependencies:
+````bash
+npm install
+````
+3. Create a .env file in the rool of the project and add the following lines:
+````bash
+DATABASE_URL="postgresql://world_owner:E0TYLo2RWuUz@ep-delicate-queen-a1gxmygl.ap-southeast-1.aws.neon.tech/books_read?sslmode=require"
+PORT="3000"
+````
+5. Run the application:
+````bash
+node index.js
+````
+6. Usage
+Visit http://localhost:3000 in your browser to start the quiz.
---
+++ index.js
@@ -0,0 +1,149 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+const db = new pg.Client({
+    connectionString:process.env.DATABASE_URL
+});
+
+db.connect();
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+async function readData(sortOrder = 'id', genre='',search=''){
+    var books = {
+        isbn: [],
+        name: [],
+        author: [],
+        rating: [],
+        genre: [],
+        localISBN: []
+    };
+    try{
+        let result;
+        if(!genre && !search){
+            result = await db.query(`SELECT isbn, name, author, rating, genre, local_img_isbn FROM books ORDER BY ${sortOrder}`);
+        } else if(genre){ 
+            result = await db.query(`SELECT isbn, name, author, rating, genre, local_img_isbn FROM books WHERE genre = '${genre}' ORDER BY ${sortOrder}`);
+        } else if(search){
+            result = await db.query(`SELECT isbn, name, author, rating, genre, local_img_isbn FROM books WHERE LOWER(name) LIKE LOWER($1) ORDER BY ${sortOrder}`,[`%${search}%`]);
+        }
+        const arr = result.rows;
+        arr.forEach(element => {
+            books.isbn.push(element.isbn);
+            books.name.push(element.name);
+            books.author.push(element.author);
+            books.rating.push(element.rating);
+            books.genre.push(element.genre.toUpperCase());
+            books.localISBN.push(element.local_img_isbn);
+        });
+    } catch(err){
+        console.log(err);
+        throw err;
+    }
+    return books;
+}
+
+async function readDetail(identity) {
+    var books = {
+        isbn: null,
+        name: null,
+        author: null,
+        rating: null,
+        genre: null,
+        localISBN: null
+    };
+    try {
+        let result = await db.query(`SELECT isbn, name, author, rating, genre, local_img_isbn FROM books WHERE isbn = '${identity}'`);
+        let arr = result.rows;
+        console.log(arr);
+
+        if (arr.length === 0) {
+            result = await db.query(`SELECT isbn, name, author, rating, genre, local_img_isbn FROM books WHERE local_img_isbn = '${identity}'`);
+            arr = result.rows;
+            console.log(arr);
+        }
+
+        if (arr.length > 0) {
+            books.isbn = arr[0].isbn;
+            books.name = arr[0].name;
+            books.author = arr[0].author;
+            books.rating = arr[0].rating;
+            books.genre = arr[0].genre.toUpperCase();
+            books.localISBN = arr[0].local_img_isbn;
+        }
+    } catch(err) {
+        console.log(err);
+        throw err;
+    }
+    return books;
+}
+
+
+app.get("/",async (req,res)=>{
+    const books = await readData();
+    res.render("index.ejs",{isbn:books.isbn, genre:books.genre, name:books.name, author:books.author, rating:books.rating, localISBN:books.localISBN});
+});
+
+app.get("/rating", async (req,res) => {
+    const books = await readData('rating DESC');
+    res.render("index.ejs",{isbn:books.isbn, genre:books.genre, name:books.name, author:books.author, rating:books.rating, localISBN:books.localISBN});
+});
+
+app.get("/alphabetically", async (req,res) => {
+    const books = await readData('name');
+    res.render("index.ejs",{isbn:books.isbn, genre:books.genre, name:books.name, author:books.author, rating:books.rating, localISBN:books.localISBN});
+});
+
+app.get("/fiction", async(req,res) =>{
+    const books = await readData('rating DESC','fiction');
+    res.render("index.ejs",{isbn:books.isbn, genre:books.genre, name:books.name, author:books.author, rating:books.rating, localISBN:books.localISBN});
+});
+
+app.get("/nonfiction", async(req,res) =>{
+    const books = await readData('rating DESC','nonfiction');
+    res.render("index.ejs",{isbn:books.isbn, genre:books.genre, name:books.name, author:books.author, rating:books.rating, localISBN:books.localISBN});
+});
+
+app.get("/autobiography", async(req,res) =>{
+    const books = await readData('rating DESC','autobiography');
+    res.render("index.ejs",{isbn:books.isbn, genre:books.genre, name:books.name, author:books.author, rating:books.rating, localISBN:books.localISBN});
+});
+
+app.get("/selfhelp", async(req,res) =>{
+    const books = await readData('rating DESC','self-help');
+    res.render("index.ejs",{isbn:books.isbn, genre:books.genre, name:books.name, author:books.author, rating:books.rating, localISBN:books.localISBN});
+});
+
+app.get("/search", async(req,res) =>{
+    const search = req.query.search;
+    const books = await readData('rating','',search);
+    if(books){
+        res.render("index.ejs",{isbn:books.isbn, genre:books.genre, name:books.name, author:books.author, rating:books.rating, localISBN:books.localISBN});
+    }
+    else
+        res.render("index.ejs");
+});
+
+app.get("/details",async(req,res)=>{
+    const books = await readData();
+    const identity = req.query.isbn;
+    const book_detail = await readDetail(identity);
+    console.log(book_detail);
+    res.render("review.ejs",{isbn:books.isbn, isbn_web:book_detail.isbn, localISBN:book_detail.localISBN, name: book_detail.name, author: book_detail.author, rating: book_detail.rating});
+});
+
+app.post("/search", async (req,res)=>{
+    res.redirect(`/search?search=${encodeURIComponent(req.body.search)}`);
+});
+
+app.listen(port, ()=>{
+    console.log(`Server listening on port ${port}`);
+});
